2ad66285f35bba64a83b335f9284de511ebca926
sanjaybankar12 <sanjaybankar12@gmail.com>
2023-07-09 21:53:37 +0530

    non repeating letter other way

--- src/com/utility/FirstNonRepeatingLetter.java
+++ src/com/utility/FirstNonRepeatingLetter.java
@@ -25,11 +25,32 @@ public class FirstNonRepeatingLetter {
 		return letter;
 	}
 	
+	public String findFirstNonRepeatingLetterEasy(String str) {
+		String letter = EMPTY_STRING;
+		
+		String[] arr = str.split("");
+		int idx = 0, lastIdx = 0;
+		for(int i  = 0; i < arr.length; i++) {
+			String el = arr[i];			
+			idx = str.indexOf(el);
+			lastIdx = str.lastIndexOf(el);
+			if(idx == lastIdx) {
+				letter = el;
+				break;
+			}
+			
+		}		
+		return letter;
+	}
+	
 	public static void main(String[] args) {
 
 		String s = "welcome to Pune";
 		String firstLetter = new FirstNonRepeatingLetter().findFirstNonRepeatingLetter(s);
 		System.out.println("First non repeating letter in '" + s + "' is " + firstLetter);
+		
+		String firstLetterEasy = new FirstNonRepeatingLetter().findFirstNonRepeatingLetterEasy(s);
+		System.out.println("First non repeating letter in '" + s + "' is " + firstLetterEasy);
 	}
 	
 }
