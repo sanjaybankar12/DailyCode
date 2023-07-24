f015a9e11ef47bdc0d711dc40c04819106507869
sanjaybankar12 <sanjaybankar12@gmail.com>
2023-07-24 22:35:02 +0530

    Immutable class example

--- src/com/utility/ImmutableExample.java
+++ src/com/utility/ImmutableExample.java
@@ -4,23 +4,50 @@ package com.utility;
  * Once you created object of immutable class then object value can not be changed.
  *
  */
-final class Employee {
+final class Employee implements Cloneable {
 	private final String name;
 	
-	public Employee(String name) {
+	private final Address adr;
+	
+	public Employee(String name, Address address) {
 		this.name = name;
+		Address tempAdr = new Address();
+		tempAdr.setCity(address.getCity());
+		tempAdr.setState(address.getState());
+		this.adr = tempAdr;
 	}
 	
 	public String getName() {
 		return this.name;
 	}
+	
+	public Address getAddress() {
+		Address ad = null;
+		try {
+			ad = (Address) this.adr.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return ad;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", adr=" + adr + "]";
+	}
+		
 }
 
 public class ImmutableExample {
 
 	public static void main(String[] args) {
-		//
-		Employee e = new Employee("hello");
+		Address adr = new Address("Pune","MH");
+		Employee e = new Employee("hello", adr);		
+		System.out.println(e);
+		
+		e.getAddress().setCity("Mumbai");
+		System.out.println(e);
+		
 		
 	}
 
